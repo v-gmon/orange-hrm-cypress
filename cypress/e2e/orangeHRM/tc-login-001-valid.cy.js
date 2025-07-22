26371d0ef0d2ebcb1e3dd0ec0f585bba68381909
Vasu <faculty-tech@facultydigital.com>
2025-07-22 14:46:06 +0700

    feat:intercept

--- cypress/e2e/orangeHRM/tc-login-001-valid.cy.js
+++ cypress/e2e/orangeHRM/tc-login-001-valid.cy.js
@@ -1,5 +1,8 @@
 describe('OrangeHRM Login Test', () => {
   it('should login with valid credentials', () => {
+    cy.intercept('POST', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/validate').as('validateLogin');
+    cy.intercept('POST', 'https://opensource-demo.orangehrmlive.com/web/index.php/events/push').as('eventRequest');
+    cy.intercept('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login').as('loginRequest');
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
     cy.get('input[name="username"]').should('be.visible');
@@ -9,6 +12,10 @@ describe('OrangeHRM Login Test', () => {
     cy.get('input[name="password"]').type('admin123');
 
     cy.get('button[type="submit"]').click();
+
+    cy.wait('@validateLogin').its('response.statusCode').should('eq', 302);
+    cy.wait('@eventRequest').its('response.statusCode').should('eq', 200);
+    cy.wait('@loginRequest').its('response.statusCode').should('eq', 200);
 
     cy.url().should('include', '/dashboard');
     cy.get('h6').should('contain', 'Dashboard');
